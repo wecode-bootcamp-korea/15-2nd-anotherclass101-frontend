a02c29bf2189ca420e79a4a3ffb3699ddf2e321a
mina <yegrinaaa@gmail.com>
2020-12-29 19:40:22 +0900

    초기세팅

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./Components/Footer/Footer";
-import Nav from "./Components/Nav/Nav";
-import Main from "./Pages/Main/Main";
-import Mypage from "./Pages/Mypage/"
-import Creator from "./Pages/Creator/Creator";
-import SignIn from "./Pages/SignIn/SignIn";
-import SignUp from "./Pages/SignUp/SignUp";
-import Detail from "./Pages/Detail/Detail";
-import Kids from "./Pages/Kids/Kids";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
+import Nav from './Components/Nav/Nav';
+import Main from './Pages/Main/Main';
+import Mypage from './Pages/Mypage/';
+import Creator from './Pages/Creator/Creator';
+import Auth from './Pages/Auth/Auth';
+import Detail from './Pages/Detail/Detail';
+import Kids from './Pages/Kids/Kids';
 
 class Routes extends React.Component {
   render() {
@@ -19,10 +18,9 @@ class Routes extends React.Component {
           <Route exact path="/" component={Main} />
           <Route exact path="/Creator" component={Creator} />
           <Route exact path="/Mypage" component={Mypage} />
-          <Route exact path="/SignIn" component={SignIn} />
-          <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/Auth" component={Auth} />
           <Route exact path="/Detail" component={Detail} />
-          <Route exact path="/Kids" component={Kids}/>
+          <Route exact path="/Kids" component={Kids} />
         </Switch>
         <Footer />
       </Router>
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+* {
+    text-decoration: none;
+    box-sizing: border-box;
+  }
+html{
+    overflow-y:scroll;
+    overflow-x:hidden;
+}
+input {
+    border-style: none;
+    cursor: pointer;
+  }
+a {
+    text-decoration: none;
+  }
+button {
+    border-style: none;
+    cursor: pointer;
+  }
+
+  `;
+export default GlobalStyles;
